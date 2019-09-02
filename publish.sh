45a1a06626ac35bb28d8ffe162d271428de960bb
nlimpid <jshuangzl@gmail.com>
2019-09-02 15:22:06 +0800

    test

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ git remote -v
 
 echo "Updating gh-pages branch"
 cd nlimpid.github.io
-git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/nlimpid.github.io.git"
+git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/nlimpid/nlimpid.github.io.git"
 git remote -v
 git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 git push github master
