011b6faff101e2f6dd41845cf618bac8626e0076
nlimpid <jshuangzl@gmail.com>
2019-09-02 14:31:55 +0800

    test

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -7,5 +7,7 @@ sh -c "ls -la"
 sh -c "ls -lah themes/paper"
 sh -c "pwd"
 sh -c "/usr/local/bin/hugo"
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "----> Build is complete"
