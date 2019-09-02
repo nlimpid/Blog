5d7a9469de1383c43973ca3b0b8fa27f66174d0a
nlimpid <jshuangzl@gmail.com>
2019-09-02 14:56:29 +0800

    test

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 
 #!/bin/sh
+git config --global user.email "jshuangzl@gmail.com"
+git config --global user.name "nlimpid"
 
 echo "Updating gh-pages branch"
 cd nlimpid.github.io && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
